f6b7500b64aaf55ec83f86c059b0fd57e7abf8a1
Joshua Holbrook <josh.holbrook@gmail.com>
2012-07-06 22:36:20 -0800

    [fix] path.existsSync was moved to fs.existsSync

--- test/exports.js
+++ test/exports.js
@@ -2,7 +2,7 @@ var test = require('tap').test;
 var exportify = require('../');
 var fs = require('fs');
 var path = require('path');
-var existsSync = fs.existsSync || path.existsSync;
+var existsSync = fs.existsSync || fs.existsSync;
 
 (function () {
     // clean out the directory
